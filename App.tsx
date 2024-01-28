e5667a95c589dd44a2b4c18e09897b58acebb83e
Burkes321 <sam.burke99@gmail.com>
2024-01-28 23:17:20 +0100

    create a route for adding a new workout. Add dumbbell png

--- App.tsx
+++ App.tsx
@@ -10,16 +10,32 @@ import React from 'react';
 
 import { Home } from './src/screens/Home';
 import { Exercises } from './src/screens/Exercises';
+import { Workout } from './src/screens/Workout';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { Image } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
-const plusTabIcon = ({ color, size }: { color: string; size: number }) => (
+// TODO: Create a function which generates the tab icons or move them to a separate file if not possible
+const UserTabIcon = ({ color, size }: { color: string; size: number }) => (
   <Image
     style={{ height: size, width: size, tintColor: color }}
     source={require('./src/assets/user.png')}
+  />
+);
+
+const WorkoutTabIcon = ({ color, size }: { color: string; size: number }) => (
+  <Image
+    style={{ height: size, width: size, tintColor: color }}
+    source={require('./src/assets/plus.png')}
+  />
+);
+
+const ExercisesTabIcon = ({ color, size }: { color: string; size: number }) => (
+  <Image
+    style={{ height: size, width: size, tintColor: color }}
+    source={require('./src/assets/dumbbell.png')}
   />
 );
 
@@ -31,12 +47,26 @@ function App(): React.JSX.Element {
           name="Home"
           component={Home}
           options={{
-            tabBarIcon: ({ color, size }) => {
-              return plusTabIcon({ color, size });
-            },
+            tabBarIcon: ({ color, size }) => UserTabIcon({ color, size }),
+            headerShown: false,
           }}
         />
-        <Tab.Screen name="Exercises" component={Exercises} />
+        <Tab.Screen
+          name="Workout"
+          component={Workout}
+          options={{
+            tabBarIcon: ({ color, size }) => WorkoutTabIcon({ color, size }),
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen
+          name="Exercises"
+          component={Exercises}
+          options={{
+            tabBarIcon: ({ color, size }) => ExercisesTabIcon({ color, size }),
+            headerShown: false,
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/Workout.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { StyleSheet, Text, View } from 'react-native';
+import { STYLE_CONSTANTS } from '../styleConstants';
+
+export const Workout = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Create a new workout</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: STYLE_CONSTANTS.DARK_PRIMARY_SHADE,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: STYLE_CONSTANTS.TEXT_COLOR,
+  },
+});
